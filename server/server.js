7ddbb349e3f5a435e09c9cff28a4f3d517c2a378
Rob Armitage <robert.c.armitage@gmail.com>
2020-11-27 00:04:55 +0000

    create basic Express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
-// Express server goes here
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => res.json({ msg: "Server running" }));
+
+app.listen(PORT, console.log(`Server started on ${PORT}`));
